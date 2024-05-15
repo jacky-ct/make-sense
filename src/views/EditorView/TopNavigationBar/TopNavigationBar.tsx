025cbdf45ab041ec4f456bec03c5262d1641c991
Jackson Thomas <jacksonchristopherthomas@gmail.com>
2024-05-15 11:46:47 +0100

    refactor with branding changes

--- src/views/EditorView/TopNavigationBar/TopNavigationBar.tsx
+++ src/views/EditorView/TopNavigationBar/TopNavigationBar.tsx
@@ -1,105 +1,95 @@
-import React from 'react';
-import './TopNavigationBar.scss';
-import StateBar from '../StateBar/StateBar';
-import {PopupWindowType} from '../../../data/enums/PopupWindowType';
-import {AppState} from '../../../store';
-import {connect} from 'react-redux';
-import {updateActivePopupType, updateProjectData} from '../../../store/general/actionCreators';
-import TextInput from '../../Common/TextInput/TextInput';
-import {ImageButton} from '../../Common/ImageButton/ImageButton';
-import {Settings} from '../../../settings/Settings';
-import {ProjectData} from '../../../store/general/types';
-import DropDownMenu from './DropDownMenu/DropDownMenu';
-import {store} from '../../../index';
+import React from "react";
+import "./TopNavigationBar.scss";
+import StateBar from "../StateBar/StateBar";
+import { PopupWindowType } from "../../../data/enums/PopupWindowType";
+import { AppState } from "../../../store";
+import { connect } from "react-redux";
+import {
+  updateActivePopupType,
+  updateProjectData,
+} from "../../../store/general/actionCreators";
+import TextInput from "../../Common/TextInput/TextInput";
+import { ImageButton } from "../../Common/ImageButton/ImageButton";
+import { Settings } from "../../../settings/Settings";
+import { ProjectData } from "../../../store/general/types";
+import DropDownMenu from "./DropDownMenu/DropDownMenu";
+import { store } from "../../../index";
 
 interface IProps {
-    updateActivePopupTypeAction: (activePopupType: PopupWindowType) => any;
-    updateProjectDataAction: (projectData: ProjectData) => any;
-    projectData: ProjectData;
+  updateActivePopupTypeAction: (activePopupType: PopupWindowType) => any;
+  updateProjectDataAction: (projectData: ProjectData) => any;
+  projectData: ProjectData;
 }
 
 const TopNavigationBar: React.FC<IProps> = (props) => {
-    const onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-        event.target.setSelectionRange(0, event.target.value.length);
-    };
+  const onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.setSelectionRange(0, event.target.value.length);
+  };
 
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value
-            .toLowerCase()
-            .replace(' ', '-');
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toLowerCase().replace(" ", "-");
 
-        props.updateProjectDataAction({
-            ...props.projectData,
-            name: value
-        })
-    };
+    props.updateProjectDataAction({
+      ...props.projectData,
+      name: value,
+    });
+  };
 
-    const closePopup = () => props.updateActivePopupTypeAction(PopupWindowType.EXIT_PROJECT)
-    const runAI = () => store.dispatch(updateActivePopupType(PopupWindowType.LOAD_AI_MODEL))
+  const closePopup = () =>
+    props.updateActivePopupTypeAction(PopupWindowType.EXIT_PROJECT);
+  const runAI = () =>
+    store.dispatch(updateActivePopupType(PopupWindowType.LOAD_AI_MODEL));
 
-    return (
-        <div className='TopNavigationBar'>
-            <StateBar/>
-            <div className='TopNavigationBarWrapper'>
-                <div className='NavigationBarGroupWrapper'>
-                    <div
-                        className='Header'
-                        onClick={closePopup}
-                    >
-                        <img
-                            draggable={false}
-                            alt={'make-sense'}
-                            src={'ico/rfs-logo-no-text.png'}
-                        />
-                        RFS Annotator
-                    </div>
-                </div>
-                    <div
-                        className='Header'
-                        onClick={ runAI }
-                    >
-                        <img 
-                        draggable={false}
-                        alt={'AI'}
-                        src={'ico/ai-white.png'}
-                        />
-                        Run AI
-                    </div>
-                <div className='NavigationBarGroupWrapper'>
-                    <DropDownMenu/>
-                </div>
-                <div className='NavigationBarGroupWrapper middle'>
-                    <div className='ProjectName'>Project Name:</div>
-                    <TextInput
-                        isPassword={false}
-                        value={props.projectData.name}
-                        onChange={onChange}
-                        onFocus={onFocus}
-                    />
-                </div>
-                <div className='NavigationBarGroupWrapper'>
-                    <ImageButton
-                        image={'ico/github-logo.png'}
-                        imageAlt={'github-logo.png'}
-                        buttonSize={{width: 30, height: 30}}
-                        href={Settings.GITHUB_URL}
-                    />
-                </div>
-            </div>
+  return (
+    <div className="TopNavigationBar">
+      <StateBar />
+      <div className="TopNavigationBarWrapper">
+        <div className="NavigationBarGroupWrapper">
+          <div className="Header" onClick={closePopup}>
+            <img
+              draggable={false}
+              alt={"rfs"}
+              src={"ico/rfs-logo-no-text.png"}
+            />
+            RFS Annotator
+          </div>
         </div>
-    );
+        <div className="Header" onClick={runAI}>
+          <img draggable={false} alt={"AI"} src={"ico/ai-white.png"} />
+          Run AI
+        </div>
+        <div className="NavigationBarGroupWrapper">
+          <DropDownMenu />
+        </div>
+        <div className="NavigationBarGroupWrapper middle">
+          <div className="ProjectName">Project Name:</div>
+          <TextInput
+            isPassword={false}
+            value={props.projectData.name}
+            onChange={onChange}
+            onFocus={onFocus}
+          />
+        </div>
+        <div className="NavigationBarGroupWrapper">
+          <ImageButton
+            image={"ico/github-logo.png"}
+            imageAlt={"github-logo.png"}
+            buttonSize={{ width: 30, height: 30 }}
+            href={Settings.GITHUB_URL}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const mapDispatchToProps = {
-    updateActivePopupTypeAction: updateActivePopupType,
-    updateProjectDataAction: updateProjectData
+  updateActivePopupTypeAction: updateActivePopupType,
+  updateProjectDataAction: updateProjectData,
 };
 
 const mapStateToProps = (state: AppState) => ({
-    projectData: state.general.projectData
+  projectData: state.general.projectData,
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(TopNavigationBar);
+export default connect(mapStateToProps, mapDispatchToProps)(TopNavigationBar);
